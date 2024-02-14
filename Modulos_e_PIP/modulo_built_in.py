4c6ae2d3defd1fdb52acfea62daaec2d17c94baa
Gabriel Machado <gabrieldiasmachado@gmail.com>
2024-02-14 00:12:27 -0300

    Modulo os

---
+++ Modulos_e_PIP/modulo_built_in.py
@@ -0,0 +1,16 @@
+# MODULO BUILT-IN
+# O Python possui vários módulos integrados que podem ser importados e usados a qualquer momento.
+#
+# Alguns exemplos:
+# - math # - random 
+# - datetime # - os
+# - sys # - json
+# - re # - smtplib
+# - email # - tkinter
+# - socket# - http
+# - sqlite3 # - csv
+# - xml# - html
+# - urllib # - requests
+
+
+help('modules')
---
+++ Modulos_e_PIP/modulo_os.py
@@ -0,0 +1,18 @@
+# MODULO OS
+# O módulo os fornece uma maneira de usar funcionalidades dependentes do sistema operacional.
+import os
+
+# Consultar as funções disponíveis no módulo os
+# help('os')
+
+# Retonar o diretório atual
+print(f'O diretória atual é: {os.getcwd()}')
+
+# Listar arquivos e diretórios
+print(f'Os arquivos e diretórios são: {os.listdir()}')
+
+# Vesão do SO
+os.system('systeminfo')
+
+# Limpar tela
+os.system('clear')
